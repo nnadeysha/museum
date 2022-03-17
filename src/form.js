5175ff2b3bfbf37cbe5f8197af9d9d5ba28909e5
novikova.n_qschool <nadeysha23@gmail.com>
2022-03-17 23:49:59 +0400

    fix: fix data from LS

--- src/form.js
+++ src/form.js
@@ -1,27 +1,24 @@
 //FORM
 
-
 export class TicketForm {
   constructor(parent) {
     const form = parent.querySelector(".form-wrapper");
-    this.form =form;
-    this.parent =parent;
+    this.form = form;
+    this.parent = parent;
     const closeBtn = form.querySelector(".form-close-button");
     const bookBtn = form.querySelector(".booking-button");
     this.seniorTypeTicketForm = form.querySelector(".senior-form");
-    const basicTypeTicketForm = form.querySelector(".basic-form");
+    this.basicTypeTicketForm = form.querySelector(".basic-form");
     this.totalForm = form.querySelector(".total-sum-form");
-    const inputsTicketsForm = form.querySelectorAll("#tickets-buy-input");
-    const radioTypeForm = form.querySelectorAll('input[type="radio"]');
-
+    const inputsTicketsForm = form.querySelectorAll("#tickets-buy-input-form");
+    this.radioTypeForm = form.querySelectorAll('input[type="radio"]');
+    this.totalCountTicketsBasic = document.querySelector('.total-count-block--basic');
+    this.totalCountTicketsSenior = document.querySelector('.total-count-block--senior');
     const ripple = document.createElement("div");
     ripple.classList.add("ripple");
 
     ripple.style.display = "none";
     bookBtn.prepend(ripple);
-
-    
-    
 
     bookBtn.addEventListener("click", (e) => {
       const left = e.clientX - e.target.getBoundingClientRect().left;
@@ -32,18 +29,22 @@ export class TicketForm {
     });
 
     bookBtn.addEventListener("mouseleave", () => {
-      /* bookBtn.removeChild(ripple); */
       ripple.style.display = "none";
     });
 
-    
-
     closeBtn.addEventListener("click", () => this.hide());
+
+    inputsTicketsForm.forEach((el) => {
+      el.addEventListener("click", () => this.update());
+    });
   }
-  
-  show(totalPrice, seniorTypeTicket) {
+
+  show(totalPrice, seniorTypeTicket, basicTypeTicket) {
     this.totalForm.innerHTML = `Total: €${totalPrice}`;
-      this.seniorTypeTicketForm.value = seniorTypeTicket;
+    this.seniorTypeTicketForm.value = seniorTypeTicket;
+    this.basicTypeTicketForm.value = basicTypeTicket;
+    this.totalCountTicketsBasic.textContent = basicTypeTicket;
+    this.totalCountTicketsSenior.textContent = seniorTypeTicket;
     this.parent.style.display = "block";
     setTimeout(() => {
       this.form.classList.toggle("active");
@@ -53,9 +54,64 @@ export class TicketForm {
   hide() {
     this.form.ontransitionend = () => {
       this.parent.style.display = "none";
-      this.form.ontransitionend = null
+      this.form.ontransitionend = null;
     };
     this.form.classList.remove("active");
   }
+  update() {
+    const seniorTypeTicket = document.querySelector(".senior");
+    const basicTypeTicket = document.querySelector(".basic");
+    const total = document.querySelector(".total");
+    localStorage.setItem(
+      "seniorTypeTicket",
+      this.seniorTypeTicketForm.value.toString()
+    );
+    seniorTypeTicket.value = localStorage.getItem("seniorTypeTicket");
+    
+    localStorage.setItem(
+      "basicTypeTicket",
+      this.basicTypeTicketForm.value.toString()
+    );
+    basicTypeTicket.value = localStorage.getItem("basicTypeTicket");
+    this.totalCountTicketsBasic.textContent = localStorage.getItem("basicTypeTicket");
+    this.totalCountTicketsSenior.textContent =localStorage.getItem("seniorTypeTicket")
+    let totalPrice;
+    const price = {
+      radio1: 20,
+      radio2: 25,
+      radio3: 40
+    }
+    let calculate = () => {
+      for(let key in localStorage){
+        //console.log(key.slice(0, -1))
+        if(key === 'radio1'){
+          totalPrice =
+            (parseInt(seniorTypeTicket.value) * parseInt(price.radio1)) / 2 +
+            parseInt(basicTypeTicket.value) * parseInt(price.radio1);
+          
+        }
+        if(key === 'radio2'){
+          totalPrice =
+          (parseInt(seniorTypeTicket.value) * parseInt(price.radio2)) / 2 +
+          parseInt(basicTypeTicket.value) * parseInt(price.radio2);
+        }
+        if(key === 'radio3'){
+          totalPrice =
+            (parseInt(seniorTypeTicket.value) * parseInt(price.radio3)) / 2 +
+            parseInt(basicTypeTicket.value) * parseInt(price.radio3);
+        }
+      }
+      console.log(totalPrice);
+      total.innerHTML = `Total: €${totalPrice}`;
+      this.totalForm.innerHTML = `Total: €${totalPrice}`;
+      localStorage.setItem("Total", totalPrice.toString());
+         /*  totalPrice =
+            (parseInt(seniorTypeTicket.value) * parseInt(radio.value)) / 2 +
+            parseInt(basicTypeTicket.value) * parseInt(radio.value);
+      */
+    };
+    
+    calculate();
+    //localStorage.setItem("Total", totalPrice.toString());
+  }
 }
-/* new TicketForm(overlay); */
--- src/index.js
+++ src/index.js
@@ -144,7 +144,7 @@ const buyBtn = document.querySelector(".buy-button");
 
 const ticketForm = new TicketForm(overlay);
 
-buyBtn.onclick = () => ticketForm.show(totalPrice, seniorTypeTicket);
+buyBtn.onclick = () => ticketForm.show(totalPrice, localStorage.getItem("seniorTypeTicket"), localStorage.getItem("basicTypeTicket"));
 
 const seniorTypeTicket = document.querySelector(".senior");
 const basicTypeTicket = document.querySelector(".basic");
@@ -186,17 +186,20 @@ for (let i = 0; i < inputLS.length; i++) {
 function changeHandler() {
   if (this.type == "radio") {
     localStorage.setItem(this.name, this.checked);
+    
   }
 }
 
+
 document
   .querySelector('input[name="radio1"]')
-  .addEventListener("click", (e) => {
+  .addEventListener("click", (e) => { 
     localStorage.removeItem("radio2");
     localStorage.removeItem("radio3");
     document.querySelector('input[name="radio2"]').checked = false;
     document.querySelector('input[name="radio3"]').checked = false;
   });
+ 
 document
   .querySelector('input[name="radio2"]')
   .addEventListener("click", (e) => {
@@ -215,6 +218,7 @@ document
   });
 
 function checkStor() {
+  
   for (let i = 0; i < inputLS.length; i++) {
     if (inputLS[i].type === "radio") {
       inputLS[i].checked = localStorage.getItem(inputLS[i].name);
@@ -231,191 +235,25 @@ function checkStor() {
   }
 }
 checkStor();
-
-//разбить на модули не бол 100 строк
-
-/* let images = [];
-  for (let i = 1; i < 16; i++) {
-    let img = `<img src="images/gallery/galery${i}.jpg" alt="gallery${i}">`
-    images.push(img)
+window.onload = () =>{
+  if(!localStorage.getItem("radio1") && !localStorage.getItem("radio2") && !localStorage.getItem("radio3")){
+    document.querySelector('input[name="radio1"]').checked = true;
   }
   
-  let i = 0, arr = [], temp;
-  
-  while (arr.length < 15) {
-    temp = Math.trunc(Math.random() * 15 + 1);
-    if (!arr.includes(temp)) {
-      arr.push(temp);
-    };
-  }
-  let randomImages = [];
-  for (let i = 0; i < 15; i++) {
-    randomImages.push(images[arr[i] - 1])
-  }
-  
-  galleryImgContainer.innerHTML = randomImages.join('');
-   */
-
-//art gallery surfacing
-
-/* 
-  const video = document.querySelector('.video');
-  const playBtn = document.querySelector('.video-play');
-  const playBigBtn = document.querySelector('.video-play-big-btn');
-  const time = document.querySelector('.control-time');
-  const mute = document.querySelector('.volume-icon');
-  
-  const fullscreenbtn = document.querySelector('.scale-icon');
-  
-  //progress volume
-  mute.onclick = vidmute;
-  function vidmute(){
-    if(video.muted){
-      video.muted = false;
-      mute.classList.remove('muteon');
-      mute.style.backgroundImage = "url(./images/btn-volume.svg)";
-    } else {
-      video.muted = true;
-      mute.classList.toggle('muteon');
-      mute.style.backgroundImage = "url(./images/mute.svg)";
-    }
-  }
-  
- 
-
-  progressVolume.oninput = videoVolume;
-  function videoVolume() {
-  video.volume = this.value/100;
-  
-  if(video.volume > 0){
-    mute.style.backgroundImage = "url(./images/btn-volume.svg)";
-  } else {
-    mute.style.backgroundImage = "url(./images/mute.svg)";
-  }
-    const value = this.value;
-    this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, grey 100%)`
-  
-  }
-  
-  //progress video
-  progress.oninput = setProgress;
-  function setProgress() {
-    video.currentTime = (progress.value * video.duration) / 100;
-    progress.value = (video.currentTime / video.duration) * 100
+  if(!localStorage.getItem("seniorTypeTicket") ){
+    localStorage.setItem("seniorTypeTicket", 0);
     
-    progress.style.background = `linear-gradient(to right, #710707 0%, #710707 ${progress.value}%, #C4C4C4 ${progress.value}%, grey 100%)`
+  } 
+  if(!localStorage.getItem("basicTypeTicket")){
+    localStorage.setItem("basicTypeTicket", 0);
   }
   
-  
-  
-    //timer
-   function updateProgress(){
-     progress.value = (video.currentTime / video.duration) * 100
-  
-     //minutes
-      let minutes = Math.floor(video.currentTime / 60);
-      if(minutes < 10) {
-        minutes = '0' + String(minutes)
-      }
-     //seconds
-     let seconds = Math.floor(video.currentTime % 60)
-     if(seconds < 10) {
-      seconds = '0' + String(seconds)
-    }
-  
-     time.innerHTML = `${minutes}:${seconds}`
-   }
-  
-  
-    //play & pause
-  playBtn.onclick = updateToggle;
-    function updateToggle() {
-      if (video.paused) {
-       playBtn.style.backgroundImage = "url(./images/btn-play-mini.svg)";
-       playBigBtn.style.display = 'block';
-      } else if (video.play) {
-        playBtn.style.backgroundImage = "url(./images/pause.svg)";
-        playBigBtn.style.display = 'none';
-        video.playbackRate =1;
-      }
-    }
-  
-   function togglePlay() {
-     const method = video.paused ? 'play' : 'pause';
-     video[method]()
-   }
-  
-  //rewind
-  
-  progress.onclick = videoRewind;
-  function videoRewind(event) {
-    let w = this.offsetWidth;
-    let o = event.offsetX;
-    this.value = 100 * o/w;
-    video.pause();
-    video.currentTime = video.duration * (o/w)
-  }
-   
-  
-  //keyboard events
-  
-  document.body.onkeyup = function(e){
-    if(e.keyCode == 32){
-      const method = video.paused ? 'play' : 'pause';
-     video[method]()
-    } 
-    if(e.keyCode == 77) {
-      video.muted = !video.muted;
-      
-    }
-    if(e.keyCode == 70) {
-      if(toggleFullScreen() == false) {
-        toggleFullScreen()
-      } if (document.exitFullscreen) {
-        document.exitFullscreen()
-      }
-    }
-    if(e.keyCode == 188 & e.shiftKey ) {
-      video.play();
-      video.playbackRate = 1.5;
-    }
-    if(e.keyCode == 190 & e.shiftKey ) {
-      video.play();
-      video.playbackRate = 0.5;
-      
-    }
-   
-}
+  calculate();
+};
+
+//localStorage.setItem("radio1", true);
 
 
 
-  document.documentElement.addEventListener('keydown', function (e) {
-    if ( ( e.keycode || e.which ) == 32) {
-        e.preventDefault();
-    }
-}, false);
 
-
-   //fullscreen
-
-   function toggleFullScreen(){
-    if(video.requestFullScreen){
-      video.requestFullScreen();
-    } else if(video.webkitRequestFullScreen){
-      video.webkitRequestFullScreen();
-    } else if(video.mozRequestFullScreen){
-      video.mozRequestFullScreen();
-    }
-
-  }
-  
-  
-  fullscreenbtn.addEventListener("click",toggleFullScreen,false);
-playBtn.addEventListener('click', togglePlay);
-video.addEventListener('click', togglePlay);
-playBigBtn.addEventListener('click', togglePlay);
-video.addEventListener('play', updateToggle);
-video.addEventListener('pause', updateToggle);
-video.addEventListener('timeupdate', updateProgress);
-progress.addEventListener('change', setProgress) 
-*/
+//localStorage.clear()
